9d4861e84ff14e82202a26980b29ff3d76bed3ef
Rebeca Rguez <72274516+rrrguez@users.noreply.github.com>
2023-12-11 18:22:39 +0000

    Update PassengerTest.java with Doxygen comments

--- src/test/java/es/ull/passengers/PassengerTest.java
+++ src/test/java/es/ull/passengers/PassengerTest.java
@@ -1,3 +1,16 @@
+/**
+ * @file PassengerTest.java
+ * @brief This file contains JUnit tests for the Passenger class.
+ *
+ * The PassengerTest class includes various test cases to ensure the proper
+ * functionality of the Passenger class, covering scenarios such as getters,
+ * invalid country codes, joining and leaving flights, failure to join a flight,
+ * and verifying the toString method.
+ *
+ * @author Rebeca Rguez Rguez
+ * @date 11/12/2023
+ */
+
 package es.ull.passengers;
 
 import es.ull.flights.Flight;
@@ -7,7 +20,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
+/**
+ * @class PassengerTest
+ * @brief Contains JUnit tests for the Passenger class.
+ *
+ * The PassengerTest class includes various test cases to verify the correct
+ * behavior of the Passenger class methods, such as getters, handling invalid
+ * country codes, joining and leaving flights, failure to join a flight, and
+ * the toString method.
+ */
 public class PassengerTest {
+
+    /**
+     * @brief Tests getters for the Passenger class.
+     */
     @Test
     public void testGetters() {
         Passenger passenger = new Passenger("ID123", "John Doe", "US");
@@ -16,11 +42,17 @@ public class PassengerTest {
         assertEquals("US", passenger.getCountryCode());
     }
 
+    /**
+     * @brief Tests creating a Passenger object with an invalid country code.
+     */
     @Test
     public void testInvalidCountryCode() {
         assertThrows(RuntimeException.class, () -> new Passenger("ID123", "John Doe", "Invalid"));
     }
 
+    /**
+     * @brief Tests joining a flight for a Passenger.
+     */
     @Test
     public void testJoinFlight() {
         Flight flight = new Flight("AB123", 2);
@@ -30,6 +62,9 @@ public class PassengerTest {
         assertEquals(1, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Tests leaving the previous flight and joining a new flight for a Passenger.
+     */
     @Test
     public void testLeavePreviousFlight() {
         Flight flight1 = new Flight("AB123", 2);
@@ -42,6 +77,9 @@ public class PassengerTest {
         assertEquals(1, flight2.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Tests joining a flight that exceeds its available seats.
+     */
     @Test
     public void testJoinFlightFails() {
         Flight flight1 = new Flight("AB123", 1);
@@ -51,6 +89,9 @@ public class PassengerTest {
         assertThrows(RuntimeException.class, () -> passenger2.joinFlight(flight1));
     }
 
+    /**
+     * @brief Tests the toString method for the Passenger class.
+     */
     @Test
     public void testToString() {
         Passenger passenger1 = new Passenger("ID123", "John Doe", "US");
@@ -58,5 +99,4 @@ public class PassengerTest {
         assertEquals("Passenger John Doe with identifier: ID123 from US", passenger1.toString());
         assertEquals("Passenger Jane Doe with identifier: ID456 from CA", passenger2.toString());
     }
-
 }
