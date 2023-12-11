7ca4ed1eaee71af0f2b6357917c31ab557ad6022
Rebeca Rguez <72274516+rrrguez@users.noreply.github.com>
2023-12-11 18:20:48 +0000

    Update FlightTest.java with Doxygen comments

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -1,3 +1,16 @@
+/**
+ * @file FlightTest.java
+ * @brief This file contains JUnit tests for the Flight class.
+ *
+ * The FlightTest class includes various test cases to ensure the proper
+ * functionality of the Flight class, covering scenarios such as creating
+ * flights, adding and removing passengers, handling invalid flight numbers,
+ * and checking seat limits.
+ *
+ * @author Rebeca Rguez Rguez
+ * @date 11/12/2023
+ */
+
 package es.ull.flights;
 
 import es.ull.passengers.Passenger;
@@ -6,19 +19,36 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * @class FlightTest
+ * @brief Contains JUnit tests for the Flight class.
+ *
+ * The FlightTest class includes various test cases to verify the correct
+ * behavior of the Flight class methods, such as adding and removing passengers,
+ * validating flight numbers, and handling seat limits.
+ */
 public class FlightTest {
 
+    /**
+     * @brief Tests the creation of a Flight object with a valid flight number.
+     */
     @Test
     public void testValidFlightNumber() {
         Flight flight = new Flight("AB123", 100);
         assertEquals("AB123", flight.getFlightNumber());
     }
 
+    /**
+     * @brief Tests the creation of a Flight object with an invalid flight number.
+     */
     @Test
     public void testInvalidFlightNumber() {
         assertThrows(RuntimeException.class, () -> new Flight("Invalid", 100));
     }
 
+    /**
+     * @brief Tests adding a passenger to a flight with available seats.
+     */
     @Test
     public void testAddPassenger() {
         Flight flight = new Flight("AB123", 2);
@@ -27,6 +57,9 @@ public class FlightTest {
         assertEquals(1, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Tests adding a passenger to a flight exceeding its available seats.
+     */
     @Test
     public void testAddPassengerExceedsSeats() {
         Flight flight = new Flight("AB123", 1);
@@ -36,6 +69,9 @@ public class FlightTest {
         assertThrows(RuntimeException.class, () -> flight.addPassenger(passenger2));
     }
 
+    /**
+     * @brief Tests removing a passenger from a flight.
+     */
     @Test
     public void testRemovePassenger() {
         Flight flight = new Flight("AB123", 2);
@@ -45,77 +81,83 @@ public class FlightTest {
         assertEquals(0, flight.getNumberOfPassengers());
     }
 
+    /**
+     * @brief Tests joining a new flight when removing the passenger from the previous flight fails.
+     */
     @Test
     public void joinFlight_RemovePassengerFails() {
-        // Test when removing passenger from previous flight fails
+        /// Test when removing passenger from the previous flight fails
         Passenger passenger = new Passenger("ID123", "John Doe", "US");
-        Flight flight = new Flight("AB123", 10); // Utiliza un número de vuelo y cantidad de asientos apropiados
-        Flight previousFlight = new Flight("CD456", 5); // Utiliza un número de vuelo y cantidad de asientos apropiados
+        Flight flight = new Flight("AB123", 10);
+        Flight previousFlight = new Flight("CD456", 5);
         passenger.setFlight(previousFlight);
 
-        // Override removePassenger to simulate failure
-        Flight flightWithRemoveFailure = new Flight("EF789", 8); // Utiliza un número de vuelo y cantidad de asientos apropiados
-        flightWithRemoveFailure = new Flight("EF789", 8) {
+        /// Override removePassenger to simulate failure
+        Flight flightWithRemoveFailure = new Flight("EF789", 8) {
             @Override
             public boolean removePassenger(Passenger passenger) {
-                return false; // Simula el fallo al eliminar el pasajero
+                return false; /// Simulates failure to remove the passenger
             }
         };
         passenger.setFlight(flightWithRemoveFailure);
 
-        // Assert that joining the new flight throws RuntimeException
+        /// Assert that joining the new flight throws RuntimeException
         assertThrows(RuntimeException.class, () -> passenger.joinFlight(flight));
     }
 
+    /**
+     * @brief Tests joining a new flight when adding the passenger to the new flight fails.
+     */
     @Test
     public void joinFlight_AddPassengerFails() {
-        // Test when adding passenger to the new flight fails
+        /// Test when adding passenger to the new flight fails
         Passenger passenger = new Passenger("ID123", "John Doe", "US");
         Flight flightWithAddFailure = new Flight("GH101", 2) {
             @Override
             public boolean addPassenger(Passenger passenger) {
-                return false; // Simula el fallo al agregar el pasajero
+                return false; /// Simulates failure to add the passenger
             }
         };
 
-        // Assert that joining the new flight throws RuntimeException
+        /// Assert that joining the new flight throws RuntimeException
         assertThrows(RuntimeException.class, () -> passenger.joinFlight(flightWithAddFailure));
     }
-    
+
+    /**
+     * @brief Tests a mixed scenario involving multiple flights and passengers.
+     */
     @Test
     public void testMixedScenario() {
-        // Create flights
+        /// Create flights
         Flight flight1 = new Flight("AB123", 2);
         Flight flight2 = new Flight("CD456", 1);
 
-        // Create passengers
+        /// Create passengers
         Passenger passenger1 = new Passenger("ID123", "John Doe", "US");
         Passenger passenger2 = new Passenger("ID456", "Jane Doe", "CA");
         Passenger passenger3 = new Passenger("ID789", "Alice Smith", "GB");
 
-        // Join passengers to flights
+        /// Join passengers to flights
         passenger1.joinFlight(flight1);
         passenger2.joinFlight(flight1);
 
-        // Ensure passengers are in the correct flights
+        /// Ensure passengers are in the correct flights
         assertEquals(flight1, passenger1.getFlight());
         assertEquals(flight1, passenger2.getFlight());
         assertNull(passenger3.getFlight());
 
-        // Ensure the number of passengers in each flight is correct
+        /// Ensure the number of passengers in each flight is correct
         assertEquals(2, flight1.getNumberOfPassengers());
         assertEquals(0, flight2.getNumberOfPassengers());
 
-        // Try to add a passenger to a full flight
+        /// Try to add a passenger to a full flight
         passenger1.joinFlight(flight2);
         assertThrows(RuntimeException.class, () -> flight2.addPassenger(passenger3));
         passenger1.joinFlight(flight1);
 
-        // Remove a passenger from a flight
+        /// Remove a passenger from a flight
         assertTrue(flight1.removePassenger(passenger1));
         assertNull(passenger1.getFlight());
         assertEquals(1, flight1.getNumberOfPassengers());
     }
 }
-
-
